128906b74b59dae2b7e8ce9ed73d873f58f7366a
Ashwin <ashwinrajiv@gmail.com>
2009-02-13 23:09:22 +0530

    added dbcheck utilities

---
+++ dbchecks.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'dbman.php';
+function auth_user($username,$password){
+    $dbcon=new dbman("localhost","root","");
+    $dbcon->make_connect("rowz");
+    $query = "select uid from rowz_users where uid='".$username."' and password=password('".$password."')";
+    //echo $query;
+    $result=$dbcon->exec_query($query);
+    if($result==null){
+	echo "here";
+        return -1;
+    }
+    else{
+    $row = mysql_fetch_assoc($result);
+     return $row['uid'];
+    }
+}
+
+function check_user($username){
+   $dbcon=new dbman("localhost","root","");
+   $dbcon->make_connect("rowz");
+   $query = "select uid from rowz_users where uid='".$username."'";
+   $result=$dbcon->exec_query($query);
+    if($result!=null&&mysql_num_rows($result)==1){
+        echo "here";
+        return 1;
+    }
+   else return 0;
+}
+
+print check_user("ashwin");
+?>
+
+
